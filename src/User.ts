c0eb3e01c228f65d0195e2ae44641a96f8fed692
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-12-12 12:06:25 -0800

    created user class

--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,24 @@
 class User{
-    
+    username: string;
+    password: string;
+    age: number;
+    loggedIn: boolean;
+    constructor(username: string, password: string, age: number){
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.loggedIn = false;
+    }
+
+    login(password: string): void{
+        if(this.password === password){
+            this.loggedIn = true;
+        }else{
+            throw Error("Incorrect Password!")
+        }
+    }
+
+    logout(): void{
+        this.loggedIn = false;
+    }
 }
