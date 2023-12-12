253bcb949dbfb3affa3d7513af5dd254b28cf9d8
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-12-12 14:06:00 -0800

    logout user and createScooter

--- src/ScooterApp.ts
+++ src/ScooterApp.ts
@@ -27,6 +27,31 @@ export class ScooterApp {
     }
 
     loginUser(username: string, password: string): void{
-        
+        if(this.registeredUsers[username]){
+            this.registeredUsers[username].login(password);
+            console.log("User has been logged in.")
+        }else{
+            throw Error("Username or password is incorrect");
+        }
+    }
+
+    logoutUser(username: string): void{
+        if(this.registeredUsers[username]){
+            this.registeredUsers[username].logout();
+            console.log("User has been logged out.")
+        }else{
+            throw Error("No such user is logged in.");
+        }
+    }
+    createScooter(station: string): Scooter{
+        if(this.stations[station]){
+            let made = new Scooter(station);
+            this.stations[station].push(made);
+            console.log("Created a new scooter.");
+            console.log(this.stations);
+            return made;
+        }else{
+            throw Error("No such station!");
+        }
     }
 }
